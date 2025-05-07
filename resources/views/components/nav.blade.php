646a723d1f4e6221b4c4ca1eb0ff5c4727e903d5
naufalham <hambalinaufal4@gmail.com>
2025-05-07 15:54:39 +0700

    perbaikan nav

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,5 +3,19 @@
     <a class="hover:underline" href="/berita">Berita</a>
     <a class="hover:underline" href="/kalkulator">Kalkulator</a>
     <a class="hover:underline" href="/usaha">Usaha</a>
-    <a class="text-[#F97316] hover:underline" href="/login">Masuk</a>
+    @auth
+        <a class="text-[#F97316] hover:underline" href="/profil">
+            {{ Auth::user()->nama }}
+        </a>
+        <form method="POST" action="{{ route('logout') }}" class="inline">
+            @csrf
+            <button type="submit" class="text-[#F97316] hover:underline">
+                Logout
+            </button>
+        </form>
+    @else
+        <a class="text-[#F97316] hover:underline" href="/login">
+          Masuk
+        </a>
+    @endauth
 </nav>
