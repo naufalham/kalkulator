a0f9fe9c3e23203b29ae11bf2f4cba28c86988ea
Pamela <pamel4515@gmail.com>
2025-05-08 15:02:05 +0700

    ui apa

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,20 +1,20 @@
 <nav class="flex flex-wrap justify-center sm:justify-end space-x-6 font-semibold text-black text-sm w-full sm:w-auto">
-    <a class="hover:underline" href="/">Beranda</a>
-    <a class="hover:underline" href="/berita">Berita</a>
-    <a class="hover:underline" href="/kalkulator">Kalkulator</a>
-    <a class="hover:underline" href="/usaha">Usaha</a>
+    <a class="{{ Request::is('/') ? 'text-[#F97316]' : '' }} hover:underline" href="/">Beranda</a>
+    <a class="{{ Request::is('berita') ? 'text-[#F97316]' : '' }} hover:underline" href="/berita">Berita</a>
+    <a class="{{ Request::is('kalkulator') ? 'text-[#F97316]' : '' }} hover:underline" href="/kalkulator">Kalkulator</a>
+    <a class="{{ Request::is('usaha') ? 'text-[#F97316]' : '' }} hover:underline" href="/usaha">Usaha</a>
     @auth
-        <a class="text-[#F97316] hover:underline" href="/profil">
+        <a class="{{ Request::is('profil') ? 'text-[#F97316]' : '' }} hover:underline" href="/profil">
             {{ Auth::user()->nama }}
         </a>
         <form method="POST" action="{{ route('logout') }}" class="inline">
             @csrf
-            <button type="submit" class="text-[#F97316] hover:underline">
+            <button type="submit" class="hover:underline">
                 Logout
             </button>
         </form>
     @else
-        <a class="text-[#F97316] hover:underline" href="/login">
+        <a class="{{ Request::is('login') ? 'text-[#F97316]' : '' }} hover:underline" href="/login">
           Masuk
         </a>
     @endauth
