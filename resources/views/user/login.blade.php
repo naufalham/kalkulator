272a0794dba3e76874e70532200ff6b76fde7e65
naufalham <hambalinaufal4@gmail.com>
2025-05-14 11:18:18 +0700

    propill

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -31,33 +31,6 @@
           <span class="font-medium">Login dengan Google</span>
         </a>
 
-    <!-- Header -->
-    <x-navbar></x-navbar>
-  
-    <!-- Main content -->
-    <main class="flex-grow flex justify-center items-start mt-12 px-4 sm:px-6">
-      <section aria-label="Form login NaZMaLogy" class="bg-white rounded-2xl max-w-md w-full p-6 sm:p-8 shadow-sm">
-        <h1 class="font-semibold text-center text-lg">
-          Selamat Datang!
-        </h1>
-        <form class="flex flex-col space-y-5" method="POST" action="{{ route('login.post') }}">
-
-          @if ($errors->any())
-              <div class="bg-red-100 text-red-700 p-4 rounded-md mb-4">
-                  <ul>
-                      @foreach ($errors->all() as $error)
-                          <li>{{ $error }}</li>
-                      @endforeach
-                  </ul>
-              </div>
-          @endif
-          @csrf
-          <input class="w-full border border-black rounded-lg px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-orange-500" placeholder="Email" required="" type="email" name="email" value="{{ old('email') }}"/>
-          <input class="w-full border border-black rounded-lg px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-orange-500" placeholder="Kata Sandi" required="" type="password" name="password" value="{{ old('password') }}"/>
-          <button class="bg-[#F97316] text-white font-semibold rounded-lg py-3 mt-2 hover:bg-[#e06f11] transition-colors" type="submit">
-            Masuk
-          </button>
-
         </form>
 
       <p class="text-center text-black text-sm font-semibold mt-4">
